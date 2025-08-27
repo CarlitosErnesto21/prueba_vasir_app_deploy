41d750b39a8419c757db01e346327a9a24650075
CarlitosErnesto21 <ernesto.rosales354@gmail.com>
2025-08-27 10:39:47 -0600

    Creación de seeder para asignación de role admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            RolePermissionAndSiteSettingsSeeder::class,
+            RolePermissionSeeder::class,
             SiteSettingsSeeder::class,
         ]);
     }
